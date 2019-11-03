16bd2d22f543b33d67c90dcf6066327af47fd83b
melvin roest <melvinroest@gmail.com>
2019-11-03 19:35:19 +0100

    correct user model in its email_opt_out validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   # Validations
-  validates_presence_of :fullname, :username, :email, :password_digest, :email_opt_out
+  validates_presence_of :fullname, :username, :email, :password_digest
+  validates_inclusion_of :email_opt_out, in: [true, false]
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,6 @@ FactoryBot.define do
     username { Faker::Internet.username }
     email { Faker::Internet.email }
     password { Faker::Internet.password }
-    email_opt_out { Faker::Boolean.boolean(true_ratio: 0.5) }
+    email_opt_out { Faker::Boolean.boolean }
   end
 end

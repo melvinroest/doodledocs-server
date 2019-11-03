4fbc8047a324c4e6c351a8da59cbbb63c3997f26
melvin roest <melvinroest@gmail.com>
2019-11-03 17:39:42 +0100

    initial test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
+# Test suite for User model
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with the Todo model
+  # it { should have_many(:todos) }
+  # Validation tests
+  # ensure name, email and password_digest are present before save
+  it { should validate_presence_of(:fullname) }
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:email_opt_out)}
+
 end

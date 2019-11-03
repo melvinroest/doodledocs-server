90680aa5da2f766f0e3b6550273f8811acee5a1a
melvin roest <melvinroest@gmail.com>
2019-11-03 17:57:21 +0100

    added user model and installed bcrypt for green tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    # encrypt password
+  has_secure_password
+
+  # Validations
+  validates_presence_of :fullname, :username, :email, :password_digest, :email_opt_out
 end

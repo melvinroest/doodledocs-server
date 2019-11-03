23eec21a48a8d5f0d0aeab21b390a1631fe3bfae
melvin roest <melvinroest@gmail.com>
2019-11-03 17:52:37 +0100

    initial users.rb factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
 # spec/factories/users.rb
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.name }
-    email 'foo@bar.com'
-    password 'foobar'
+    fullname { Faker::Name.name }
+    username { Faker::Internet.username }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    email_opt_out { Faker::Boolean.boolean(true_ratio: 0.5) }
   end
 end
